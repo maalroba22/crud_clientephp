57679f51a415fa29ac9253131b9e14a2c071daa1
maalroba22 <maalroba22@gmail.com>
2022-10-02 13:12:00 -0500

    conifguracion de conexion

---
+++ config/datos.php
@@ -0,0 +1,9 @@
+<?php
+  $datos = [
+    "host"     => "localhost",
+    "user"     => "root",
+    "pass"     => "",
+    "db"       => "clientes",
+    "path"     => "",
+    "key"      => ''
+  ];
